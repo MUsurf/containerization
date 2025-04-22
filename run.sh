80cf8b8ba9d30998c50b29938e60b75ece9c4f0b
MizzouSURF <160983057+MizzouSURF@users.noreply.github.com>
2025-04-22 18:41:10 -0500

    Create imu launch file

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 source /opt/ros/$ROS_DISTRO/setup.bash && \
-  source install/local_setup.bash && \
-  # python3 ./src/motor_tester/motor_listener.py
+  source install/local_setup.bash
+  # python3 ./src/motor_command/motor_command/motor_listener.py
   # ros2 launch motor_command motors.py
   # ros2 run motor_command motor_runner
   # ros2 run motor_command motor_listener
-  ros2 run imu bno055_i2c_node
-  
+  ros2 launch ./src/imu/launch/imu.xml
+
+  # ros2 launch py_launch launch.yaml
